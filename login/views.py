d03db7aff0c117966ee6713b401f7e37d29ee88b
khattabhassan <42593613+khattabhassan@users.noreply.github.com>
2022-02-07 19:07:43 +0200

    Update views.py

--- login/views.py
+++ login/views.py
@@ -6,6 +6,18 @@ from .forms import StudentForm
 def index(request): 
     return render (request, 'index.html')
 
+def schooladmin(request): 
+    return render (request, 'school-admin.html')
+
+def datastatement(request): 
+    return render (request, 'data-statement.html')
+
+def browse(request): 
+    return render (request, 'browse.html')
+
+def about(request): 
+    return render (request, 'about.html')
+
 def showformschool(request):
     form= SchoolForm(request.POST or None)
     if form.is_valid():
